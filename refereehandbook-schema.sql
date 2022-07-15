5329c58442cfa3c52824f1d21dd662390fc25ff3
travisnava <trn5106@psu.edu>
2022-07-15 13:18:34 -0400

    Added users table to db, created methods to generate JWT token for users in utils/tokens.js

--- refereehandbook-schema.sql
+++ refereehandbook-schema.sql
@@ -1,6 +1,16 @@
--- CREATE TABLE users (
-
--- )
+-- CREATE USER TABLE
+CREATE TABLE users (
+    id          SERIAL PRIMARY KEY,
+    email       TEXT NOT NULL UNIQUE CHECK(POSITION('@' IN email) > 1),
+    username    TEXT NOT NULL,
+    password    TEXT NOT NULL,
+    first_name  TEXT NOT NULL,
+    last_name   TEXT NOT NULL,
+    location    TEXT NOT NULL,
+    profile_image_URL TEXT,
+    created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at  TIMESTAMP NOT NULL DEFAULT NOW()
+);
 
 
 
