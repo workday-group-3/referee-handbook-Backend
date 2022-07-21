490e397cbef456081e71b41b710a9b70bb7dd9b6
travisnava <trn5106@psu.edu>
2022-07-21 12:06:56 -0400

    Added comments

--- routes/profile.js
+++ routes/profile.js
@@ -6,10 +6,10 @@ const Profile = require("../models/profile")
 
 
 
-//List all user created courses for the current sport course list
+//List all user created courses owned by the currently signed in user
 router.get("/", async (req, res, next) => {
     try {
-        // send json response back for list of all user created courses under sportName category
+        // send json response back for list of all user created courses that are owned by the currently signed in user
         const { user } = res.locals
         const userOwnedCourses = await Profile.listUserCoursesByUser(user)
         return res.status(200).json({ userOwnedCourses })
